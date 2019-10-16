3b5ebf9adbb5d8ba088b7773164316dfd136bb6d
Ip15 <56157390+Ip15@users.noreply.github.com>
2019-10-16 11:47:02 +0300

    Update Led.ino

--- Led.ino
+++ Led.ino
@@ -1,13 +1,13 @@
 void setup() {
-    pinMode(7, OUTPUT);
+    pinMode(3, OUTPUT);
 }
 void loop() {
     for(int i=0; i<255; i++){
-        analogWrite(7, i);
+        analogWrite(3, i);
         delay(20);
     }
     for(int i=255;i>0;i--){
-        analogWrite(7, i);
+        analogWrite(3, i);
         delay(20);
     }
 
